0bd74e959e1cd896bd6af5fae77d6be3df93ad8d
marcos <mvsb2@discente.ifpe.edu.br>
2023-10-24 18:59:03 -0300

    estudando python

--- estudo.py
+++ estudo.py
@@ -1,4 +1,5 @@
+
 nome = "Marcos Vinicius de Santana Barros"
 curso = "desenvolvimento de sistemas"
 polo = "polivalente vitoria de santo antão"
-print ("Eu me chamo", nome, "meu curso é", curso,"meu polo é",polo)
+print ("Eu me chamo", nome, "meu curso é", curso,"\n", "Meu polo é",polo)
