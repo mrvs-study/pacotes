4cba4399fbb2fd87b1153dfcbdae9541fac92349
marcos <mvsb2@discente.ifpe.edu.br>
2023-10-25 18:20:37 -0300

    not python

--- estudo.py
+++
@@ -1,8 +0,0 @@
-"""Construa um programa em Python que realize o cadastramento das entradas de
-dados, com os tipos de dados nome, idade e peso, e a na saída de dados mostre o seu
-nome, idade e peso."""
-nome = input ("digite seu nome: ")
-idade = input("digite sua idade: ")
-peso = input("digite o seu peso: ")
-
-print ("seu nome é:"+nome,"\n""sua idade é:"+idade,"\n""seu peso é:"+peso)
